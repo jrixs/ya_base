75b0a2b7c0afe47ea712d1faa4cc0d03f19e4682
Andrey12321 <nikas_0604@mail.ru>
2024-06-21 19:54:37 +0300

    add models v1

--- fastapi/src/models/film.py
+++ fastapi/src/models/film.py
@@ -1,8 +1,32 @@
 # Используем pydantic для упрощения работы при перегонке данных из json в объекты
+from datetime import datetime, date
+from typing import List
 from pydantic import BaseModel
 
-
+# Модель для фильмов
 class Film(BaseModel):
     id: str
     title: str
     description: str
+    premiere_date: date
+    type: str
+    rating: float
+    file_path: str
+    created: datetime
+    modified: datetime
+
+# Модель для жанров
+class Genre(BaseModel):
+    id: str
+    name: str
+    description: str
+    created: datetime
+    modified: datetime
+
+# Модель для людей (актеров, сценаристов, режиссеров)
+class Person(BaseModel):
+    id: str
+    full_name: str
+    gender: str
+    created: datetime
+    modified: datetime

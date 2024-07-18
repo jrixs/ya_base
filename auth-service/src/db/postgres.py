691713a9dca9eafc1df3102bdccb63f740809791
Yrui Synovets <jrix@yandex.ru>
2024-07-18 21:05:19 +0300

    Добавление контейнеров с базовой конфигурацикй

--- auth-service/src/db/postgres.py
+++ auth-service/src/db/postgres.py
@@ -6,9 +6,25 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 Base = declarative_base()
 # Создаём движок
 # Настройки подключения к БД передаём из переменных окружения, которые заранее загружены в файл настроек
-dsn = f'postgresql+asyncpg://{settings.user}:{settings.password}@{settings.host}:{settings.port}/{settings.db}'
+dsn = f'postgresql+asyncpg://{settings.db_user}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}'
 engine = create_async_engine(dsn, echo=True, future=True)
 async_session = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
-)  
+)
 
+
+# Функция понадобится при внедрении зависимостей
+# Dependency
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
+
+
+async def create_database() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def purge_database() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
